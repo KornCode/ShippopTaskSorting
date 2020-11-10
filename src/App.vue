4d2f29adc62567fd11d1640a5633bc76e7705ad3
Korn Chotpitakkul <korn.chot@gmail.com>
2020-11-10 16:46:59 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 
         <v-spacer></v-spacer>
 
-        <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <v-btn href="https://github.com/KornCode/ShippopTaskSorting" target="_blank" text>
             <span class="mr-2">GITHUB</span>
-            <v-icon>mdi-open-in-new</v-icon>
+            <v-icon>mdi-github</v-icon>
         </v-btn>
     </v-app-bar>
 
