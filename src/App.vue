bf6e4a3f1ceb3887fc2ced3d07447fca5a435ddf
Korn Chotpitakkul <korn.chot@gmail.com>
2020-11-11 12:16:13 +0700

    second commit

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,15 @@
         <v-spacer></v-spacer>
 
         <v-btn href="https://github.com/KornCode/ShippopTaskSorting" target="_blank" text>
-            <span class="mr-2">GITHUB</span>
+            <span class="mr-1">GITHUB</span>
             <v-icon>mdi-github</v-icon>
+        </v-btn>
+
+        <v-divider inset vertical></v-divider>
+
+        <v-btn href="https://www.facebook.com/korn.chotpitakkul/" target="_blank" text>
+            <span class="mr-1">FACEBOOK</span>
+            <v-icon>mdi-facebook</v-icon>
         </v-btn>
     </v-app-bar>
 
